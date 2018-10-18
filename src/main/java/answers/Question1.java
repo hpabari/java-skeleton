4834e3c5da001c11d4bd3cd572a6632f45c89c0d
unknown <henapabari@hotmail.com>
2018-10-18 20:26:18 +0100

    Question 1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,8 +3,16 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+		int n = 0;
+		
+		for(int i=0; i<portfolios.length; i++){
+			for(int j=0; j<portfolios.length; j++){
+				if((portfolios[i] ^ portfolios[j]) > n){
+					n = (portfolios[i] ^ portfolios[j]);
+				}
+			}
+		}
+		return n;
 	}
 
 }
