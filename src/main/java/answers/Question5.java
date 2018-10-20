79bedff6f814e6080386fda8c1d3e4cf41e0103a
unknown <henapabari@hotmail.com>
2018-10-20 15:39:24 +0100

    Question 5 attempt

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -3,8 +3,27 @@ package answers;
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		// TODO Auto-generated method stub
-		return -1;
+		Arrays.sort(allowedAllocations);
+		
+		int output = 0;
+		
+		for(int i=0; i<allowedAllocations.length; i++) {
+			if(totalValue == allowedAllocations[i]) {
+				output = 1;
+			}
+			else {
+				breakLoop:
+				for(int k=0; k<allowedAllocations.length; k++){
+					for(int j=0; j<allowedAllocations.length; j++){
+						output = 2;
+						if(allowedAllocations[k] + allowedAllocations[j] == totalValue) {
+							break breakLoop;
+						}
+					}
+				}
+			}
+		}
+		return output;
 	}
 
 }
