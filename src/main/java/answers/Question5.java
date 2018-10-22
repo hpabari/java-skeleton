7c413e50f8efb79b83db8d07a6f185e0dbe0fe63
hpabari <henapabari@hotmail.com>
2018-10-22 15:50:40 +0100

    Chnaged back to previous

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -7,42 +7,26 @@ public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		
-		ArrayList<ArrayList<Integer>> allCombinations = new ArrayList<ArrayList<Integer>>();
-
-		ArrayList<Integer> workingArray = new ArrayList<Integer>();
-		
 		Arrays.sort(allowedAllocations);
-
-		getAllCombinations(allowedAllocations, totalValue, workingArray, allCombinations);
-	 
+		
 		int output = 0;
-
-		if(!allCombinations.isEmpty()){
-			output = allCombinations.get(0).size();
-			for(int i=0; i<allCombinations.size(); i++){
-				if(allCombinations.get(i).size() < output){
-					output = allCombinations.get(i).size();
+		
+		for(int i=0; i<allowedAllocations.length; i++) {
+			if(totalValue == allowedAllocations[i]) {
+				output = 1;
+			}
+			else {
+				breakLoop:
+				for(int k=0; k<allowedAllocations.length; k++){
+					for(int j=0; j<allowedAllocations.length; j++){
+						output = 2;
+						if(allowedAllocations[k] + allowedAllocations[j] == totalValue) {
+							break breakLoop;
+						}
+					}
 				}
 			}
 		}
 		return output;
-	}
-
-	public static void getAllCombinations(int[] allowedAllocations, int totalValue, ArrayList<Integer> workingArr, ArrayList<ArrayList<Integer>> allCombinations){
-		
-		if(totalValue == 0){
-			ArrayList<Integer> temp = new ArrayList<Integer>(workingArr);
-			allCombinations.add(temp);
-			return;
-		}
-		
-		for(int i=0; i<allowedAllocations.length; i++){
-			if(totalValue < allowedAllocations[i]){
-				return;
-			}
-			workingArr.add(allowedAllocations[i]);
-			getAllCombinations(allowedAllocations, totalValue - allowedAllocations[i], workingArr, allCombinations);
-			workingArr.remove(workingArr.size()-1);
-		}
 	}
 }
