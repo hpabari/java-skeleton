288a065402928dcc377424f59ebb53cb827d7831
hpabari <henapabari@hotmail.com>
2018-10-22 13:07:18 +0100

    Question 4 attempt

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -1,10 +1,40 @@
 package answers;
 
+import java.util.*;
+
 public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return -1;
-	}
+		
+		int repairTime = 0;
+		int tracknumber = 0;
+		
+		Set<Integer> possibleRepairs = new HashSet<Integer>(); 
 
+		for(int i=0; i<rows.length; i++){
+			for(int j=0; j<rows[i].length; j++){
+				if(!rows[i][j].equals("X")){
+					repairTime += Integer.parseInt(rows[i][j]);
+					tracknumber++;
+					if(tracknumber == numberMachines){
+						possibleRepairs.add(repairTime);
+					}
+				}
+				else{
+					repairTime = 0;
+					tracknumber = 0;
+				}
+			}
+			repairTime = 0;
+			tracknumber = 0;
+		}
+		
+		int output = 0;
+		
+		if(!possibleRepairs.isEmpty()){
+			output = Collections.min(possibleRepairs);
+		}
+
+		return output;
+	}
 }
