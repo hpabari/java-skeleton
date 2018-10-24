1a193ce2e4528364ec3068122ced51280b61c9e1
hpabari <henapabari@hotmail.com>
2018-10-24 11:53:11 +0100

    Question 6

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -1,10 +1,24 @@
 package answers;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class Question6 {
 
 	public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
-		// TODO Auto-generated method stub
-		return -1;
+		Set <Integer> routes = new HashSet<>();
+		routes.add(times[0][targetServer]);
+		
+		for(int j=0; j<numServers; j++){
+			if(j != targetServer){
+				int otherServer = times[0][j];
+				routes.add(otherServer + times[j][targetServer]);
+			}
+		}
+
+		int output = Collections.min(routes);
+	
+		return output;
 	}
 
 }
