6c4b48f4b45da434fa7c4eb8d2fba147dd70d2c3
hpabari <henapabari@hotmail.com>
2018-10-29 11:37:51 +0000

    Question 5

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -21,7 +21,7 @@ public class Question5 {
 		Set<Integer> shares = new HashSet<Integer>();
 		
 		breakLoop:
-		if(output == 0) {	
+		if(output == 0) {
 			for(int k=0; k<allowedAllocations.length; k++){
 				for(int j=0; j<allowedAllocations.length; j++){
 					int addedShares = allowedAllocations[k] + allowedAllocations[j];
@@ -35,13 +35,27 @@ public class Question5 {
 				}
 			}
 
-			breakloop2:
-			for(int m:shares){
+			Set<Integer> shares2 = new HashSet<Integer>();
+
+			for(int s:shares){
 				for(int n=0; n<allowedAllocations.length; n++){
-					int addedShares = m + allowedAllocations[n];
+					int addedShares = s + allowedAllocations[n];
 					if(addedShares == totalValue) {
 						output = 3;
-						break breakloop2;
+						break breakLoop;
+					}
+					if(addedShares < totalValue){
+						shares2.add(addedShares);
+					}
+				}
+			}
+
+			for(int s:shares2){
+				for(int m=0; m<allowedAllocations.length; m++){
+					int addedShares = s + allowedAllocations[m];
+					if(addedShares == totalValue) {
+						output = 4;
+						break breakLoop;
 					}
 				}
 			}
